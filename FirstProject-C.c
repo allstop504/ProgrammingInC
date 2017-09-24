156834c29e61452ad6d1c8ffb7edacc8a5851253
McKamey <bmckamey@lp-bmckam-osx.b33rb3lly>
2017-09-23 20:00:59 -0700

    updated

--- FirstProject-C.c
+++ FirstProject-C.c
@@ -1,3 +1,12 @@
+/* --------------------------------------------------------------------------
+Program file:   bmckamep1.c
+Author:         Buddy Mckamey
+Date:           September 21, 2017
+Assignment:     #1
+Objective:      Calculates and prints the sum, difference, product, quotient,
+                and remainder of two integer numbers
+---------------------------------------------------------------------------*/
+
 #include <stdio.h>
 
 int main(void)
