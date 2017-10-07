83b2de387940de579772f4aa4ec592c71ea200ab
McKamey <bmckamey@lp-bmckam-osx.b33rb3lly>
2017-10-06 18:40:42 -0700

    new file

---
+++ bmckamep2.c
@@ -0,0 +1,11 @@
+/* --------------------------------------------------------------------------
+
+Program file:   bmckamep2.c
+Author:         Buddy Mckamey
+Date:           October 6, 2017
+Assignment:     #2
+Objective:      Program Looping
+
+---------------------------------------------------------------------------*/
+
+#include <stdio.h>
